c97ec4bc33383b6905371ceb128eff6e9e1cab8f
ArseniiTkachuk <ryunoskyeakutagawa@gmail.com>
2026-04-27 11:58:13 +0300

    Полагодив бекенд

--- backend/Presentation/utils/passport.js
+++ backend/Presentation/utils/passport.js
@@ -1,6 +1,6 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import { User } from "./../Infractructure/DataBase/index.js";
+import { User } from "./../../Infractructure/DataBase/index.js";
 import dotenv from "dotenv";
 
 dotenv.config();
